9245ca84b5649d567782b0423e43e3a66c8fcc88
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-24 16:23:18 +0200

    finalizza header

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,8 +1,23 @@
 export default function MyHeader() {
 return (<header> 
-            <div>
-                <img src="./img/dc-logo.png" alt="" />
+            <div className="header-box">
+                <div>
+                    <img src="./img/dc-logo.png" alt="" />
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="">Characters</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">Tv</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Collectibles</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">Fans</a></li>
+                        <li><a href="">News</a></li>
+                        <li><a href="">Shop</a></li>
+                    </ul>
+                </div>
             </div>
-            <div></div>
         </header>);
 }
