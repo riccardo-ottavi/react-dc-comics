7667d0763e8bf18d12c78faf1d0912b561b5e415
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-24 17:26:51 +0200

    finalizza inserimento elementi footer

--- src/components/VerticalNav.jsx
+++ src/components/VerticalNav.jsx
@@ -1,8 +1,51 @@
 export default function VericalNav() {
 return (<div className="vertical-nav-box">
-            <img src="./img/footer-bg.jpg" alt="" />
             <div className="vertical-nav">
-
+                <div className="vertical-nav-section">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                    </ul>
+                </div>
+                <div className="vertical-nav-section">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                    </ul>
+                </div>
+                <div className="vertical-nav-section">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                    </ul>
+                </div>
+                <div className="vertical-nav-section">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                        <li><a href="">characters</a></li>
+                    </ul>
+                </div>
+                <img src="./img/dc-logo-bg.png" alt="" />
             </div>
         </div>);
 }
