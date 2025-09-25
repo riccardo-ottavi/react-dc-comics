a73fae0af04ee14b07f8d39352ffb1116f766fa0
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-25 17:52:36 +0200

    crea un componente per la navbar

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,23 +1,12 @@
+import MyNavbar from "./MyNavbar"
+
 export default function MyHeader() {
 return (<header> 
             <div className="header-box">
                 <div>
                     <img src="./img/dc-logo.png" alt="" />
                 </div>
-                <div>
-                    <ul>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Comics</a></li>
-                        <li><a href="">Movies</a></li>
-                        <li><a href="">Tv</a></li>
-                        <li><a href="">Games</a></li>
-                        <li><a href="">Collectibles</a></li>
-                        <li><a href="">Videos</a></li>
-                        <li><a href="">Fans</a></li>
-                        <li><a href="">News</a></li>
-                        <li><a href="">Shop</a></li>
-                    </ul>
-                </div>
+                <MyNavbar></MyNavbar>
             </div>
         </header>);
 }
---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,18 @@
+export default function MyNavbar(){
+    return(
+        <nav>
+            <ul>
+                <li><a href="">Characters</a></li>
+                <li><a href="">Comics</a></li>
+                <li><a href="">Movies</a></li>
+                <li><a href="">Tv</a></li>
+                <li><a href="">Games</a></li>
+                <li><a href="">Collectibles</a></li>
+                <li><a href="">Videos</a></li>
+                <li><a href="">Fans</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">Shop</a></li>
+            </ul>
+        </nav>
+    )
+}
