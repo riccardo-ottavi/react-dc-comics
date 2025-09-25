d85d2f7e215cd5c4a50fdc7280eb025ad42be64d
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-25 19:01:04 +0200

    finalizza dinamicità

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,4 +1,4 @@
-const DcLinks = [
+const dcLinks = [
     { label :"Characters", href:"#" },
     { label :"Comics", href:"#" },
     { label :"Movies", href:"#" },
@@ -11,21 +11,13 @@ const DcLinks = [
     { label :"Shop", href:"#" }
 ]
 
-export default function MyNavbar(){
-    return(
-        <nav>
-            <ul>
-                <li><a href="">Characters</a></li>
-                <li><a href="">Comics</a></li>
-                <li><a href="">Movies</a></li>
-                <li><a href="">Tv</a></li>
-                <li><a href="">Games</a></li>
-                <li><a href="">Collectibles</a></li>
-                <li><a href="">Videos</a></li>
-                <li><a href="">Fans</a></li>
-                <li><a href="">News</a></li>
-                <li><a href="">Shop</a></li>
-            </ul>
-        </nav>
-    )
+export default function MyNavbar() {
+    return (
+        <ul>
+            {dcLinks.map((link) => (
+                <li>{link.label}</li>
+            ))}
+        </ul>
+    );
 }
+
