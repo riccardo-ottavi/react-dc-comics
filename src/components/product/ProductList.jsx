89f4faaf02e680c25ea30982880b19ff85195506
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-25 16:22:48 +0200

    inserisci i fumetti in pagina

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -2,6 +2,25 @@ import comics from "./comics"
 
 export default function ProductList(){
     return(
-            <h1>Provaaa</h1>
+        <>
+            <div className="jumbo-container"></div>
+            <section id="products">
+            
+
+            <div className="container card-container">
+                <h2>Lista prodotti</h2>
+                {/* Card di prodotto  */}
+                {comics.map(comics => (
+                    <div key={comics.title} className="prod-card">
+                        <img src={comics.thumb} alt={comics.title} />
+                        <h3>{comics.title}</h3>
+                    </div>
+                ))}
+
+
+            </div>
+        </section>
+        </>
+            
     )
 }
