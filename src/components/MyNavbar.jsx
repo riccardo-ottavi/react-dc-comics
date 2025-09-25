15965caa6d924e3b09aa1f9a85db69493f9c8e8c
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-25 18:05:38 +0200

    inizializza dinamicità della navbar

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,3 +1,16 @@
+const DcLinks = [
+    { label :"Characters", href:"#" },
+    { label :"Comics", href:"#" },
+    { label :"Movies", href:"#" },
+    { label :"Tv", href:"#" },
+    { label :"Games", href:"#" },
+    { label :"Collectibles", href:"#" },
+    { label :"Videos", href:"#" },
+    { label :"Fans", href:"#" },
+    { label :"News", href:"#" },
+    { label :"Shop", href:"#" }
+]
+
 export default function MyNavbar(){
     return(
         <nav>
